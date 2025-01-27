13f1a302f3117ab1afb77b9d9780666b9b9aac78
crisalbaws <146770231+crisalbaws@users.noreply.github.com>
2025-01-27 17:45:27 -0600

    new design

--- src/app/layout/curriculum/curriculum.component.ts
+++ src/app/layout/curriculum/curriculum.component.ts
@@ -1,20 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatCardModule } from '@angular/material/card';
-import { MatCard } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-curriculum',
   standalone: true,
-  imports: [MatDividerModule, MatChipsModule, MatIconModule, MatTooltipModule, MatCardModule, RouterModule],
+  imports: [MatDividerModule, MatDividerModule,
+    MatExpansionModule, MatChipsModule, MatIconModule, MatTooltipModule, MatCardModule, RouterModule, CommonModule],
   templateUrl: './curriculum.component.html',
-  styleUrl: './curriculum.component.scss'
+  styleUrl: './curriculum.component.scss',
 })
 export class CurriculumComponent {
+  frontendSkills = ['Angular', 'React', 'Ionic', 'TypeScript', 'Single SPA'];
+  languages = ['TypeScript', 'JavaScript', 'C#', 'Kotlin'];
+  tools = ['AWS S3', 'GitHub', 'Figma', 'Jira'];
   constructor() {
 
   }
@@ -26,5 +31,8 @@ export class CurriculumComponent {
   }
   openGitHub() {
     window.open('https://github.com/crisalbaws');
+  }
+  openCV(): void {
+    window.open('https://portfolio-caaws-public.s3.us-east-1.amazonaws.com/assets/cv_ES.pdf');
   }
 }
